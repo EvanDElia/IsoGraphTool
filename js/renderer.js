a419dcb6ea6aac6c5c263701246251bb0d293bbd
Evan D'Elia <deliaevan1@gmail.com>
2014-07-11 15:46:09 -0400

    Create renderer.js

---
+++ js/renderer.js
@@ -0,0 +1,31 @@
+function Renderer(canvas,width,height){
+	this.canvas = canvas;
+	this.context = this.canvas.getContext("2d");
+
+	this.canvas.width = width;
+	this.canvas.height = height;
+
+	console.log(this.canvas);
+}
+
+Renderer.prototype.cls = function(x,y,w,h){
+	this.context.clearRect(x,y,w,h);
+};
+
+Renderer.prototype.drawPoint = function(x,y){
+	this.context.fillRect(x - 5,y - 5,10,10);
+};
+
+Renderer.prototype.drawLine = function(x1,y1,x2,y2){
+	//this.context.strokeLine(x1,y1,x2,y2);
+	
+	this.context.beginPath();
+	this.context.moveTo(x1,y1);
+	this.context.lineTo(x2,y2);
+	this.context.stroke();
+};
+
+Renderer.prototype.drawText = function(string, x, y){
+	this.context.font = "bold 16px Arial";
+	this.context.fillText(string, x, y);
+};
